f9e43bdb42feb219d393284bc6715e2807d25b48
Salim Yahuza Gwarjo <137787045+salimyahuza@users.noreply.github.com>
2025-01-09 21:45:24 +0100

    Update Variable.py

--- Variable.py
+++ Variable.py
@@ -1,1 +1,30 @@
+# Day 2: 30 Days of python programming
+
+# Declaring variables
+first_name = "John"  # Replace "John" with your first name
+last_name = "Doe"  # Replace "Doe" with your last name
+full_name = first_name + " " + last_name
+country = "Nigeria"  # Replace "Nigeria" with your country
+city = "Katsina"  # Replace "Katsina" with your city
+age = 25  # Replace 25 with your age
+year = 2025  # Replace 2025 with the current year
+is_married = False  # Replace False with True if married
+is_true = True
+is_light_on = False
+
+# Declaring multiple variables in one line
+a, b, c = 5, 10, 15
+
+# Printing to verify the values (Optional)
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("Country:", country)
+print("City:", city)
+print("Age:", age)
+print("Year:", year)
+print("Is Married:", is_married)
+print("Is True:", is_true)
+print("Is Light On:", is_light_on)
+print("Multiple Variables:", a, b, c)
 
