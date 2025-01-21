e12270aa884d0dbe91798cb08ef2bdf2c9da5e2d
Salim Yahuza Gwarjo <137787045+salimyahuza@users.noreply.github.com>
2025-01-21 17:14:22 +0100

    Update Module 9 exercise.py

--- Module 9 exercise.py
+++ Module 9 exercise.py
@@ -204,3 +204,44 @@ def most_populated_countries(n=10):
 # Example usage:
 print(most_spoken_languages(10))
 print(most_populated_countries(10))
+
+# List comprehension exercise
+# Filter only negative and zero in the list using list comprehension
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+negative_and_zero = [num for num in numbers if num <= 0]
+print(negative_and_zero)
+
+# Flatten the list of lists of lists to a one-dimensional list
+list_of_lists = [[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+flattened_list = [num for sublist in list_of_lists for inner_list in sublist for num in inner_list]
+print(flattened_list)
+
+# Using list comprehension to create the list of tuples
+tuples_list = [(i, 1, i, i**2, i**3, i**4, i**5) for i in range(11)]
+print(tuples_list)
+
+# Flatten the list to a new list
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+flattened_countries = [[country[0].upper(), country[0][:3].upper(), country[1].upper()] for sublist in countries for country in sublist]
+print(flattened_countries)
+
+# Change the list to a list of dictionaries
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+countries_dict = [{'country': country[0].upper(), 'city': country[1].upper()} for sublist in countries for country in sublist]
+print(countries_dict)
+
+# Change the list of lists to a list of concatenated strings
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+concatenated_names = [' '.join(name) for sublist in names for name in sublist]
+print(concatenated_names)
+
+# Lambda function to solve the slope or y-intercept of linear functions
+# Slope: (y2 - y1) / (x2 - x1)
+slope = lambda x1, y1, x2, y2: (y2 - y1) / (x2 - x1) if x1 != x2 else 'undefined'
+
+# Y-intercept: y = mx + b => b = y - mx
+y_intercept = lambda x, y, m: y - m * x
+
+# Example usage
+print(slope(1, 2, 3, 4))  # Output: 1.0
+print(y_intercept(1, 2, 1))  # Output: 1
